a61d47131f570c117e82fdeceaaecfc120b7af84
Ian King <ian.antking@gmail.com>
2019-05-03 18:00:07 +0100

    added material ui

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -10,12 +32,28 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    console.log(this.state.messages);
   }
 
   render() {
-    return <h1>The system works!</h1>;
+    return (
+      this.state.messages.map(message => {
+        return (
+          <Card
+            key={message._id}
+          >
+            <CardContent>
+              <Typography>
+                {message.subject}
+              </Typography>
+              <Typography>
+                {message.author}
+              </Typography>
+            </CardContent>
+          </Card>
+        );
+      })
+    );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
