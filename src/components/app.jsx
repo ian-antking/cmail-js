098fca5f6fdba3e051efdf2e954e59ea919ffcf9
Ian King <ian.antking@gmail.com>
2019-05-03 16:51:51 +0100

    created app componetn

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return <h1>The system works!</h1>;
+  }
+}
+
+export default App;
